fd5c65d3dc8c4be6c49b01e79758482e760b6d26
ErickJulio <erickwiejulio@gmail.com>
2024-01-28 21:34:51 -0300

    ajuste video

--- cypress.config.js
+++ cypress.config.js
@@ -7,10 +7,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser, launchOptions) => {
         if (config.specs.includes('cypress/e2e/frontLogin.cy.js')) {
-          // Habilita a gravação de vídeo apenas para o teste frontLogin.cy.js
           launchOptions.record = true;
         } else {
-          // Desabilita a gravação de vídeo para outros testes
           launchOptions.record = false;
         }
 
