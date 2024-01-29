dbd945695804a1ab947c1575ed19f810edaae2a7
ErickJulio <erickwiejulio@gmail.com>
2024-01-28 21:34:06 -0300

    ajuste video

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "hu5vpz",
+  videosFolder: 'cypress/videos', // Pasta onde os vídeos serão salvos
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (config.specs.includes('cypress/e2e/frontLogin.cy.js')) {
+          // Habilita a gravação de vídeo apenas para o teste frontLogin.cy.js
+          launchOptions.record = true;
+        } else {
+          // Desabilita a gravação de vídeo para outros testes
+          launchOptions.record = false;
+        }
+
+        return launchOptions;
+      });
     },
   },
 });
