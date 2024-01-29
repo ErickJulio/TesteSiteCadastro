60dcce301faecdb3604cbcab0ad8c7c7b3ba0477
ErickJulio <erickwiejulio@gmail.com>
2024-01-28 21:42:58 -0300

    ajuste yml

--- cypress.config.js
+++ cypress.config.js
@@ -2,18 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "hu5vpz",
-  videosFolder: 'cypress/videos', // Pasta onde os vídeos serão salvos
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        if (config.specs.includes('cypress/e2e/frontLogin.cy.js')) {
-          launchOptions.record = true;
-        } else {
-          launchOptions.record = false;
-        }
-
-        return launchOptions;
-      });
+      // implement node event listeners here
     },
   },
 });
